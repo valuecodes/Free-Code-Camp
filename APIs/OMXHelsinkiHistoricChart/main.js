e01883a9a66f99cb74b6b6a43c5672d43173030f
Juha Kangas <juhakangas5@gmail.com>
2018-09-20 19:34:54 +0300

    added request

--- APIs/OMXHelsinkiHistoricChart/main.js
+++ APIs/OMXHelsinkiHistoricChart/main.js
@@ -1,8 +1,10 @@
 req=new XMLHttpRequest();
-req.open("GET",'/json/cats.json',true);
+req.open("GET",'https://raw.githubusercontent.com/valuecodes/Free-Code-Camp/master/APIs/OMXHelsinkiHistoricChart/omxhpi25.json',true);
 req.send();
 req.onload=function(){
-  json=JSON.parse(req.responseText);
-  console.log(json);
-//   document.getElementsByClassName('message')[0].innerHTML=JSON.stringify(json);
-};
+    json=JSON.parse(req.responseText);
+    console.log(json.length);
+    
+    
+  document.getElementsByClassName('visHolder')[0].innerHTML=JSON.stringify(json);
+}; 
